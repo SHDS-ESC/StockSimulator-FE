36ede01b039a6c6d113d0a6573944cb29cde4794
dlsundn <sw020406@naver.com>
2025-09-10 10:25:38 +0900

    feat  : 설정 파일

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-import Main from "./component/Main";
+import HomePage from "./pages/HomePage"; // 1. Main 대신 HomePage를 불러옵니다.
+import Character from "./pages/Character";
+import Stocks from "./pages/Stocks";
+import News from "./pages/News";
 import Login from "./component/Login";
+import "./index.css"; // 2. Tailwind CSS를 적용하기 위해 CSS 파일을 불러옵니다.
 
 function App() {
-  //const [count, setCount] = useState(0)
-
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="min-h-screen bg-gray-100 flex justify-center items-center font-['Jua']">
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/character" element={<Character />} />
+        <Route path="/stocks" element={<Stocks />} />
+        <Route path="/news" element={<News />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
